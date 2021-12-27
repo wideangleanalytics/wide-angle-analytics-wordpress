b7b502b43f9b9b04ed1033ca357cd17511d8421d
Jarek Rozanski <jrozanski@inputobjects.eu>
2021-12-27 15:21:41 +0100

    Relax PHP requirements
    A lot of installation still use PHP < 7.2

--- plugins/wide-angle-analytics/wide-angle-analytics.php
+++ plugins/wide-angle-analytics/wide-angle-analytics.php
@@ -7,7 +7,7 @@
   Author URI:           https://wideangle.co
   Version:              1.0.0
   Requires at least:    5.2
-  Requires PHP:         7.4
+  Requires PHP:         7.2
   License:              GPL v2
   License URI:          https://www.gnu.org/licenses/gpl-2.0.html
 */
